1793687e7d15c45124588bdab3568ca51f8b0e59
Joy Li <jli34448@gmail.com>
2023-06-26 10:15:30 -0400

    Initial Commit

---
+++ RED(y)/RED_y_App.swift
@@ -0,0 +1,17 @@
+//
+//  RED_y_App.swift
+//  RED(y)
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RED_y_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
